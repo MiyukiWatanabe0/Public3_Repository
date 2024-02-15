7617865e47b5ba0179ff63498f4689420bfe5cb9
MiyukiWatanabe0 <149170527+MiyukiWatanabe0@users.noreply.github.com>
2024-02-15 23:17:13 +0900

    ログイン画面に遷移出来ない

--- Kinnet/Kinnet/settings.py
+++ Kinnet/Kinnet/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-kzn0gie&bpj^%ibtol8^s(i=8lz$dt=d%+091y_qw4z(r^t1k3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
