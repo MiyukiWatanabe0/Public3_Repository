2c8f2955814be227d4b25e7eae6f94ad25abcced
MiyukiWatanabe0 <149170527+MiyukiWatanabe0@users.noreply.github.com>
2024-03-07 00:41:26 +0900

    デプロイ5

--- Kinnet/Kinnet/urls.py
+++ Kinnet/Kinnet/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("Unityapp/", include('Unityapp.urls')),
+    path("", include('Unityapp.urls')),
 ]
